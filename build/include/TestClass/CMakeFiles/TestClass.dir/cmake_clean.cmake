ec640e845e12d3449a47ba6366fc7bba80ae7e54
n-fallahinia <n.fallahinia@utah.edu>
2020-01-16 15:51:50 -0700

    add successful buil commit

---
+++ build/include/TestClass/CMakeFiles/TestClass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestClass.dir/src/TestClass.cpp.o"
+  "libTestClass.a"
+  "libTestClass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
